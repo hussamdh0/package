cdb2f24114cd4518eee741c436d52d82f85c137d
hussam_altaleb <hussam.altaleb@mailbox.tu-dresden.de>
2019-11-02 18:06:46 +0100

    settings and migrations + basic models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from core.models import Place, Shipment, User
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Place)
+admin.site.register(Shipment)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db                  import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    objects         = models.Manager()
+    name            = models.CharField(max_length=255, unique=False, null=True, blank=True)
+    creation_date   = models.DateTimeField(auto_now_add=True, blank=True, null=True, editable=False)
+    last_modified   = models.DateTimeField(auto_now=True, blank=False, null=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+    
+    class Meta:
+        abstract = True
+
+
+class BaseUserModel(AbstractUser):
+    def __str__(self):
+        return str(self.username)
+
+    class Meta:
+        abstract = True
+        
+        
+class Place(models.Model):
+    city        = models.CharField(max_length=255, unique=False, null=True, blank=True)
+    latitude    = models.FloatField(unique=False, null=True, blank=True)
+    longitude   = models.FloatField(unique=False, null=True, blank=True)
+
+
+class Shipment(BaseModel):
+    name        = models.CharField(max_length=255, unique=False, null=True, blank=True)
+    origin      = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True, related_name='shipment_origin')
+    destination = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True, related_name='shipment_destination')
+
+
+class User(BaseUserModel):
+    shipments   = models.ManyToManyField(Shipment)
