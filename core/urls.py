607fbaefdd8d29bfd20f26ecd77706a3f920f59b
hussam_altaleb <hussam.altaleb@mailbox.tu-dresden.de>
2019-11-24 22:33:13 +0100

    refactor

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from core.views import CityListAPIView, JourneyListAPIView
 app_name = "core"
 
 urlpatterns = [
-    path('login/', obtain_jwt_token, name='login'),
+    path('login', obtain_jwt_token, name='login'),
     path('city',    CityListAPIView.as_view(),     name='city'),
     path('journey', JourneyListAPIView.as_view(),  name='journey'),
 
